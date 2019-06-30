f9e00711df6cbb7dd77cab38f071dafa9e2ef24b
Drew26 <superdrewf26@hotmail.com>
2019-06-29 18:00:17 -0700

    added jawsdb to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,19 @@
-    
-
+var mysql = require('mysql')   
 var Sequelize = require("sequelize");
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'burgers_db'
+  });
+};
+
+connection.connect();
 
 
 
@@ -16,3 +29,4 @@ var sequelize = new Sequelize('burgers_db', 'root', '', {
 
 
 module.exports = sequelize;
+module.exports = connection;
