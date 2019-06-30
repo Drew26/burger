daab6bfa4abc77e7543c2fe196ee5d748673d254
Drew26 <superdrewf26@hotmail.com>
2019-06-29 19:09:14 -0700

    added sequelize-heroku dependancy

--- config/connection.js
+++ config/connection.js
@@ -1,32 +1,20 @@
-var mysql = require('mysql')   
-var Sequelize = require("sequelize");
-var connection;
+var sequelize = require('sequelize-heroku').connect(require('sequelize'));
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (sequelize) {
+    sequelize.authenticate().then( function() {
+        var config = sequelize.connectionManager.config;
+        console.log('sequelize-heroku: Connected to '+config.host+' as '+config.username+'.');
+        
+        sequelize.query('SELECT 1+1 as test').then( function(res) {
+            console.log('1+1='+res[0][0].test);
+        });
+        
+    }).catch( function(err) {
+        var config = sequelize.connectionManager.config;
+        console.log('Sequelize: Error connecting '+config.host+' as '+config.user+': '+err);
+    });
 } else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'burgers_db'
-  });
-};
-
-connection.connect();
-
-
-
-var sequelize = new Sequelize('burgers_db', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
-
+    console.log('No environnement variable found.');
+}
 
 module.exports = sequelize;
-module.exports = connection;
