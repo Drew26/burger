4533ae69d6b4067b42c33b3b8a2259e19d0e60b4
Drew26 <superdrewf26@hotmail.com>
2019-06-30 13:09:33 -0700

    connection page update

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,17 @@
-var sequelize = require('sequelize-heroku').connect(require('sequelize'));
+var mysql = require('mysql');
+var connection;
 
-if (sequelize) {
-    sequelize.authenticate().then( function() {
-        var config = sequelize.connectionManager.config;
-        console.log('sequelize-heroku: Connected to '+config.host+' as '+config.username+'.');
-        
-        sequelize.query('SELECT 1+1 as test').then( function(res) {
-            console.log('1+1='+res[0][0].test);
-        });
-        
-    }).catch( function(err) {
-        var config = sequelize.connectionManager.config;
-        console.log('Sequelize: Error connecting '+config.host+' as '+config.user+': '+err);
-    });
-} else {
-    console.log('No environnement variable found.');
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '', 
+    database : 'burgers_db'
+  });
 }
 
-module.exports = sequelize;
+
+module.exports = connection;
